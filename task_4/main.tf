1c89a14dd0107e20cba77e1527c6631dc5cc0f86
Your Name <you@example.com>
2025-11-17 02:40:44 +0300

    task4

---
+++ task_4/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  token     = "xxx"
+  cloud_id  = "xxx"
+  folder_id = "xxx"
+  zone      = "ru-central1-a"
+}
+
+data "yandex_compute_image" "ubuntu" {
+  family = "ubuntu-2204-lts"
+}
+
+resource "yandex_compute_disk" "testvm" {
+  name = "test-vm-disk"
+  type = "network-ssd"
+  zone = "ru-central1-a"
+  image_id = data.yandex_compute_image.ubuntu.image_id
+  size = 15
+}
+
+resource "yandex_compute_instance" "testvm" {
+  name = "test-vm"
+  zone = "ru-central1-a"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.testvm.id
+  }
+
+  network_interface {
+    subnet_id = "xxx"
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
